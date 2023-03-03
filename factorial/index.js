7b2223c5bfca43481ac38e57e63e17f4b7e390b3
David <dfernandez.alumno@iesrafaelalberti.es>
2023-03-03 09:07:09 +0100

    comments

--- factorial/index.js
+++ factorial/index.js
@@ -10,6 +10,7 @@ function factorial(n) {
 
 const number = window.prompt('Enter a number to calculate its factorial: ', '0')
 
+// Check if the user entered a valid number or not and display the result accordingly
 if (isNaN(number)) {
   window.alert('Invalid number')
 } else {
